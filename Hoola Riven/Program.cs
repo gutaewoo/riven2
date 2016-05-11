fb67ce5dfcf2d010e53695a39dc355bc1315c94e
gutaewoo <gutaewoo@naver.com>
2016-05-11 20:58:28 +0900

    Update Program.cs

--- Hoola Riven/Program.cs
+++ Hoola Riven/Program.cs
@@ -630,8 +630,9 @@ namespace HoolaRiven
             Player.IssueOrder(GameObjectOrder.MoveTo, Player.Position.Extend(Game.CursorPos, Player.Distance(Game.CursorPos) + 10));
         }
 
-      private static bool InWRange(GameObject target)=> Player.HasBuff("RivenFengShuiEngine") ? 330 : 265;
-        
+      private static bool InWRange(GameObject target)=> (Player.HasBuff("RivenFengShuiEngine") && target != null) ?
+                    330 >= Player.Distance(target.Position) : 265 >= Player.Distance(target.Position);
+                    
 
         private static void ForceSkill()
         {
