7f793220290569f18e332b0841dd48cfc9bfd027
gutaewoo <gutaewoo@naver.com>
2016-05-13 13:32:02 +0900

    Update Program.cs

--- Hoola Riven/Program.cs
+++ Hoola Riven/Program.cs
@@ -327,6 +327,7 @@ namespace HoolaRiven
             UseRMaxDam();
             AutoUseW();
             Killsteal();
+            if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Mixed) Harass();
             if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Burst) Burst();
             if (Orbwalker.ActiveMode == Orbwalking.OrbwalkingMode.Flee) Flee();
             if (Utils.GameTimeTickCount - LastQ >= 3650 && QStack != 1 && !Player.IsRecalling() && KeepQ && Q.IsReady()) Q.Cast(Game.CursorPos);
